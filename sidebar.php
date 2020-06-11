8cb3a0247c3ce43e16579cb3be2d024fb576b7fb
lucas sena <lsena.ls81@gmail.com>
2020-06-11 20:33:14 -0300

    att perfil

--- sidebar.php
+++ sidebar.php
@@ -10,7 +10,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="images/xxx.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Projeto X</span>
     </a>
@@ -20,7 +20,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="images/mordecai.jpg" height="160px" width="160px" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Damião Lucas</a>
